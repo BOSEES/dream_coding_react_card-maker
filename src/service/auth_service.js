849988ed611dada7c777df7c69701acb429322ff
김영일 <rladuddlf3@naver.com>
2020-10-25 19:27:19 +0900

    firebase is connect

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,9 +1,10 @@
-import firebase from "firebase";
+import firebase from 'firebase';
+import firebaseApp from './firebase';
 
-class AuthService{
-  login(providerName){
-    const authProvider = new firebase.auth[`${providerName}Authprovider`]();
-    return firebase.auth().signInWithPopup(authProvider);
+class AuthService {
+  login(providerName) {
+    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    return firebaseApp.auth().signInWithPopup(authProvider);
   }
 }
 
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,11 +1,13 @@
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH
-    databaseURL: process.env.REACT_APP_FIREBASE_URL
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
