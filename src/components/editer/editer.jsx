35bcedf703d4914173a2bb3c5de51fed4c817811
김영일 <rladuddlf3@naver.com>
2020-10-27 20:25:06 +0900

    스타일링 중

--- src/components/editer/editer.jsx
+++ src/components/editer/editer.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./editer.module.css";
+import CardEditForm from "../card_edit_form/card_edit_form";
 
-const Editer = (props) => {
+const Editer = ({cards}) => {
   return (
     <section className={styles.editer}>
       <h1 className={styles.title}>Card Maker</h1>
+      <ul>
+        {cards.map(card => (
+          <CardEditForm card={card}/>
+        ))}
+      </ul>
     </section>
   )
 }
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -5,8 +5,44 @@ import Preview from "../preview/preview";
 import Editer from "../editer/editer";
 import styles from "./maker.module.css";
 import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
 const Maker = ({authService}) => {
+  const [cards, setCards] = useState([
+    {
+      id: "bosees1",
+      name: "yeoung-il",
+      company: "none",
+      theme: "light",
+      title: "software engineer",
+      email:"rladuddlf3@aasd",
+      message:"Hello World",
+      fileName:"asdsad",
+      fileURL:null
+    },
+    {
+      id: "bosees2",
+      name: "yeoung-il",
+      company: "none",
+      theme: "light",
+      title: "software engineer",
+      email:"rladuddlf3@aasd",
+      message:"Hello World",
+      fileName:"asdsad",
+      fileURL:"asdasd"
+    },
+    {
+      id: "bosees3",
+      name: "yeoung-il",
+      company: "none",
+      theme: "light",
+      title: "software engineer",
+      email:"rladuddlf3@aasd",
+      message:"Hello World",
+      fileName:"asdsad",
+      fileURL: null
+    }
+  ]);
   const history = useHistory();
   const logout = () => {
     authService.logout();
@@ -24,8 +60,8 @@ const Maker = ({authService}) => {
     <section className={styles.maker}>
     <Header onLogout={logout}/>
     <div className={styles.container}>
-      <Editer />
-      <Preview />
+      <Editer cards={cards}/>
+      <Preview cards={cards}/>
     </div>
     <Footer />
     </section>
